c8acc8559dd030c5dc9ab8fdfe8d6d062139e6ae
Steven D'Arolfi <stevendarolfi@hotmail.com>
2020-05-04 04:04:24 -0400

    test flick settings mobile

--- component/pageTurner/pageTurner.js
+++ component/pageTurner/pageTurner.js
@@ -318,7 +318,7 @@ app.component.pageTurner.func.is.flickThresholdMet = (pxDifference, distanceThre
         ||  app.component.pageTurner.setting.page[0] === "view" &&
             pxDifference > 0 // negative = swipe to go left
         ){
-            let timeThreshold     = 150;
+            let timeThreshold     = 100;
             let timeNow           = Date.now();
             let timeDifference    = timeNow - app.component.pageTurner.setting.swipeStartTime;
             if( pxDifference < 0){
